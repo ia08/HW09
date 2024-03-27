e9356a473f73eaecc55f50e1358e1ddaa852f3e9
Isha Anand <ia5qk@virginia.edu>
2024-03-27 13:13:14 -0400

    add setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name= 'bookloverdemo',
+    version= '0.1',
+    description= 'package creation for booklover class',
+    url= 'https://github.com/ia08/HW09.git',
+    author= 'Isha Anand',
+    author_email='ia5qk@virginia.edu',
+    license= 'MIT',
+    package= ['bookloverdemo']
+)
