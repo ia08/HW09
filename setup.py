6b51bd222e0625d2e7a6c195e2ae6a3d68906528
Isha Anand <ia5qk@virginia.edu>
2024-03-30 18:39:42 -0400

    update

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
+
 from setuptools import setup
 
-
-setup(
-    name= 'bookloverdemo',
-    version= '0.1',
-    description= 'package creation for booklover class',
-    url= 'https://github.com/ia08/HW09.git',
-    author= 'Isha Anand',
-    author_email='ia5qk@virginia.edu',
-    license= 'MIT',
-    package= ['bookloverdemo']
-)
+setup(name='bookloverdemo',
+      version='0.1',
+      description='Create records of books read',
+      url='https://github.com/ia08/HW09.git',
+      author='Isha Anand',
+      author_email='ia5qk@virginia.edu',
+      license='MIT',
+      packages=['bookloverdemo'])
